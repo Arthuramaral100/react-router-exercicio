4f4cb870fd2facc0ecb05f607dbb2f94dd9e1957
Arthuramaral100 <arthuramaral57@gmail.com>
2022-11-24 09:21:41 -0300

    Exercícios de react-router

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import {goToProfilePage} from "../router/coordinator"
+
 function HomePage() {
+  const navigate = useNavigate()
+
   return (
     <section>
+      <button onClick={() => goToProfilePage(navigate, 100)} > Ir para página de perfil</button>
       <h1>Página inicial</h1>
     </section>
   );
